db50ebdf09862355fcbf361ad7ecffa082879762
lapchakd <litdim638@gmail.com>
2023-05-27 21:07:39 +0200

    Add endpoint for adding post

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('posts/', views.PostsView.PostList.as_view()),
     path('post/<int:pk>/', views.PostsView.PostDetail.as_view()),
+    path('addpost/', views.PostsView.AddPostView.as_view(), name='add_post'),
 ]
 
